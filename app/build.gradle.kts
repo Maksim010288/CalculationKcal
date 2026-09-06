f7bda4a911fd7ca4a1a0eb5ab7aae47e9fa907de
Maksym <mysik.bazyk@gmail.com>
2026-09-06 19:36:14 +0300

    test6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.calculationkcal"
         minSdk = 27
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.5"
+        versionCode = 6
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/example/calculationkcal/update/UpdateInstaller.java
+++ app/src/main/java/com/example/calculationkcal/update/UpdateInstaller.java
@@ -3,6 +3,7 @@ package com.example.calculationkcal.update;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.core.content.FileProvider;
@@ -10,7 +11,7 @@ import androidx.core.content.FileProvider;
 import java.io.File;
 
 public class UpdateInstaller {
-    public static void installApk(Context context, File apkFile) {
+    public static void installApk1(Context context, File apkFile) {
         if (!apkFile.exists()) return;
 
         // Формуємо безпечний URI через FileProvider
@@ -24,6 +25,35 @@ public class UpdateInstaller {
         // Надаємо права на читання цього файлу для інсталятора
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
+        context.startActivity(intent);
+    }
+
+    public static void installApk(Context context, File apkFile) {
+        if (apkFile == null || !apkFile.exists()) {
+            // Лог або повідомлення, що файл не знайдено
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        // Перевірка версії Android (для Android 7.0 Nougat / API 24 і вище потрібен FileProvider)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            Uri apkUri = FileProvider.getUriForFile(
+                    context,
+                    context.getPackageName() + ".fileprovider",
+                    apkFile
+            );
+            intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            // Для старих версій Android (менше API 24)
+            intent.setDataAndType(Uri.fromFile(apkFile), "application/vnd.android.package-archive");
+        }
+
+        // Прапорець обов'язковий, якщо метод викликається не з Activity (наприклад, із Service або BroadcastReceiver)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        // Запуск інсталятора
         context.startActivity(intent);
     }
 }
