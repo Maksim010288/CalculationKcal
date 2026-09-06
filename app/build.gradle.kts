e559c43ec4f00bf416cf14cb509ff2e94b95a427
Maksym <mysik.bazyk@gmail.com>
2026-09-06 20:20:58 +0300

    test8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.calculationkcal"
         minSdk = 27
         targetSdk = 36
-        versionCode = 7
-        versionName = "2.7"
+        versionCode = 8
+        versionName = "2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/example/calculationkcal/update/UpdateInstaller.java
+++ app/src/main/java/com/example/calculationkcal/update/UpdateInstaller.java
@@ -22,6 +22,9 @@ public class UpdateInstaller {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
 
+
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         // Надаємо права на читання цього файлу для інсталятора
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
